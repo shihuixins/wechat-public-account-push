b60a7dc457d993843191fa7463bfc8d707e59115
shihuixins <151846263+shihuixins@users.noreply.github.com>
2024-07-06 23:58:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝儿',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oad466gmO9LLI-CPHh7sc5RO1d6U',
+      id: 'oad466gmO9LLI-CPHh7sc5RO1d6U,oad466kKRXI2Aat_YLqYIiTcEqLs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'J5slGq4_Oss0HCspllePui-br8svpv8YgBtUWJ3q094',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'J5slGq4_Oss0HCspllePui-br8svpv8YgBtUWJ3q0940',
+  CALLBACK_TEMPLATE_ID: '	J5slGq4_Oss0HCspllePui-br8svpv8YgBtUWJ3q094',
 
   CALLBACK_USERS: [
     {
