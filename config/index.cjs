c922ea7ee7f51518e94787a18704cdada67e8578
shihuixins <151846263+shihuixins@users.noreply.github.com>
2024-07-06 22:12:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '03-05',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
@@ -42,7 +42,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2023-03-18' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'g_eNZ1vHTznfg88EuFx6yDr-kPSDFo1Zl8EtqEhBphE',
+  CALLBACK_TEMPLATE_ID: '_frQ1mQRzBss9PBHrnHEpXQkMcFZgCgxt25ih2F7J2M',
 
   CALLBACK_USERS: [
     {
