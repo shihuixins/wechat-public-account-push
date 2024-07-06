c3819cf94087bee621bc4d6e8211fd30a803ec46
shihuixins <151846263+shihuixins@users.noreply.github.com>
2024-07-07 00:38:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,6 +6,7 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
+
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wxa8675871e88e3993',
 
@@ -20,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝儿',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oad466kKRXI2Aat_YLqYIiTcEqLs'，
+      id: 'oad466kKRXI2Aat_YLqYIiTcEqLs','oad466gmO9LLI-CPHh7sc5RO1d6U'
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'J5slGq4_Oss0HCspllePui-br8svpv8YgBtUWJ3q094',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -41,7 +42,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2023-03-18' },
+        { keyword: 'love_day', date: '2022-09-08' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
@@ -50,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	J5slGq4_Oss0HCspllePui-br8svpv8YgBtUWJ3q094',
+  CALLBACK_TEMPLATE_ID: 'J5slGq4_Oss0HCspllePui-br8svpv8YgBtUWJ3q094',
 
   CALLBACK_USERS: [
     {
